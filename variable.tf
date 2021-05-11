c8e63b4f11f0cb0515ef77dfccaa6a97ccba3e66
Kishan Patel <kpatel@savanacorp.net>
2021-05-11 10:53:32 -0400

    vpc

--- variable.tf
+++ variable.tf
@@ -4,7 +4,14 @@ variable "instance_type" {
 variable "ami" {
   default = "ami-048f6ed62451373d9"
 }
-
 variable "key" {
   default = "DEC2020"
+}
+
+variable "cidr_block" {
+  default ="10.0.0.0/16"
+}
+
+variable "instance_tenancy" {
+  default ="default"
 }
