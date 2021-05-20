e46fda1cb012ad881ca440e767dac9a978680134
knpatel5900 <59032477+knpatel5900@users.noreply.github.com>
2021-05-20 00:42:05 -0400

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -1,9 +1,9 @@
 resource "aws_vpc" "main" {
-  cidr_block       = var.cidr_block
-  instance_tenancy = var.instance_tenancy
+  cidr_block       = "10.0.0.0/16"
+  instance_tenancy = "default"
 
   tags = {
-    Name = var.vpc_tags
+    Name = "default"
   }
 }
 
