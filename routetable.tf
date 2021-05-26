406b5a3db1398d67d3023f299441206dd4ef0dc6
Kishan Patel <kpatel@savanacorp.net>
2021-05-25 23:57:46 -0400

    tesr

--- routetable.tf
+++ routetable.tf
@@ -2,7 +2,7 @@ resource "aws_route_table" "Terraform_rt" {
   vpc_id = aws_vpc.main.id
 
   route {
-    cidr_block = "0.0.0.0/24"
+    cidr_block = var.route_cidr
     gateway_id = aws_internet_gateway.Terraform_igw.id
   }
 
